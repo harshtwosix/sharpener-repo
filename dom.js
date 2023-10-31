22463dc32c7f0980e1237fe8d24e1b7078c85dd3
harshtwosix <105590886+harshtwosix@users.noreply.github.com>
2023-10-31 07:33:52 +0530

    Add files via upload
    added dom file successfully

---
+++ dom.js
@@ -0,0 +1,8 @@
+//console.dir(document);
+let test=getElementsByClassName('list-group-item');
+
+test[2].style.backgroundColor='green';
+
+for(let i=0;i<test.length;i++){
+test[i].style.fontWeight='bold';
+}
