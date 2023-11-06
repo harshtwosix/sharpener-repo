5b56d9db610c0d527a90dbadd3d1277dd1611739
harshtwosix <105590886+harshtwosix@users.noreply.github.com>
2023-11-06 20:19:14 +0530

    Add files via upload
    added dom for queryselector and queryselectorALL

--- dom.js
+++ dom.js
@@ -1,8 +1,32 @@
-//console.dir(document);
-let test=getElementsByClassName('list-group-item');
+//QuerySelector
 
-test[2].style.backgroundColor='green';
+let seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color="green";
 
-for(let i=0;i<test.length;i++){
-test[i].style.fontWeight='bold';
+let thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.color="transparent";
+
+//QuerySelectorALL
+
+
+
+let odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<odd.length;i++){
+
+    odd[i].style.color="green";
+
 }
+
+let even=document.querySelectorAll('li:nth-child(even)');
+
+for(let i=0;i<even.length;i++){
+
+if(i==0){
+    even[i].style.color="red";
+}
+
+}
+
+
+
