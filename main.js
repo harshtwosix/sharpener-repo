36020afc95aff2ec1d44155c9d204505baf87ef2
harshtwosix <105590886+harshtwosix@users.noreply.github.com>
2023-12-27 18:26:37 +0530

    javascript file for appointment booking

--- main.js
+++ main.js
@@ -1,155 +1,56 @@
-// GET REQUEST
-function getTodos() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5', {timeout : 4000})
-    .then((res)=>{showOutput(res)})
-    .catch((err)=>{console.log(err)})
-  }
-  
-  // POST REQUEST
-  function addTodo() {
-    axios({
-        method :'post',
-        url: 'https://jsonplaceholder.typicode.com/todos',
-        data :{
-            name:'harshit',
-            age:23
-        }
-    })
-    .then((res)=>{showOutput(res)})
-    .catch((err)=>{showOutput(err)})
 
-  }
-  
-  // PUT/PATCH REQUEST
-  function updateTodo() {
-    axios({
-        method :'patch',
-        url: 'https://jsonplaceholder.typicode.com/todos/1',
-        data :{
-            name:'harshit prajapati',
-            age:23
-        }
-    })
-    .then((res)=>{showOutput(res)})
-    .catch((err)=>{showOutput(err)})
-  }
-  
-  // DELETE REQUEST
-  function removeTodo() {
-    axios({
-        method :'delete',
-        url: 'https://jsonplaceholder.typicode.com/todos/1'
-    })
-    .then((res)=>{showOutput(res)})
-    .catch((err)=>{showOutput(err)})
-  }
-  
-  // SIMULTANEOUS DATA
-  function getData() {
-    axios.all([
-        axios.get('https://jsonplaceholder.typicode.com/todos?limit=5'),
-        axios.get('https://jsonplaceholder.typicode.com/posts?limit=5')
-    ])
-    .then(axios.spread((todos,posts)=>showOutput(posts)))
-    .catch((err)=>showOutput(err))
-}
-  
-  // CUSTOM HEADERS
-  function customHeaders() {
-    console.log('Custom Headers');
-  }
-  
-  // TRANSFORMING REQUESTS & RESPONSES
-  function transformResponse() {
-    const options = {
-      method : 'post',
-      url : 'https://jsonplaceholder.typicode.com/todos',
-      data : {
-        title: "Hello World"
-      },
-      transformResponse: axios.defaults.transformResponse.concat(data => {
-        data.title = data.title.toUpperCase();
-        return data;
-      })
+document.getElementById('appointmentForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('contact').value;
+
+    // Create an object to represent the appointment
+    const appointment = {
+        name,
+        email,
+        phone,
     };
 
-    axios(options).then(res=>showOutput(res));
-  }
-  
-  // ERROR HANDLING
-  function errorHandling() {
-    axios.get('https://jsonplaceholder.typicode.com/todoss')
-    .then((res)=>{showOutput(res)})
-    .catch((err)=>{
-      if(err.response){
-        //server responded but page not found with a status other than 200 range
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.message);
+    // Get existing appointments from localStorage or initialize an empty array
+    const existingAppointments = JSON.parse(localStorage.getItem('appointments')) || [];
 
-        if(err.response.status===404){
-          alert("Error: the page you requested doesn't exist");
-        }
-      }else if(err.request){
-        //request was made but no response
-        console.log(err.request);
-      }
-    })
-  }
-  
-  // CANCEL TOKEN
-  function cancelToken() {
-    console.log('Cancel Token');
-  }
-  
- 
-  // AXIOS INSTANCES
-  
-  // Show output in browser
-  function showOutput(res) {
-    document.getElementById('res').innerHTML = `
-    <div class="card card-body mb-4">
-      <h5>Status: ${res.status}</h5>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Headers
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Data
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.data, null, 2)}</pre>
-      </div>
-    </div>
-  
-    <div class="card mt-3">
-      <div class="card-header">
-        Config
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.config, null, 2)}</pre>
-      </div>
-    </div>
-  `;
-  }
-  
-  // Event listeners
-  document.getElementById('get').addEventListener('click', getTodos);
-  document.getElementById('post').addEventListener('click', addTodo);
-  document.getElementById('update').addEventListener('click', updateTodo);
-  document.getElementById('delete').addEventListener('click', removeTodo);
-  document.getElementById('sim').addEventListener('click', getData);
-  document.getElementById('headers').addEventListener('click', customHeaders);
-  document
-    .getElementById('transform')
-    .addEventListener('click', transformResponse);
-  document.getElementById('error').addEventListener('click', errorHandling);
-  document.getElementById('cancel').addEventListener('click', cancelToken);
+    // Add the new appointment to the array
+    existingAppointments.push(appointment);
+
+    // Save the updated array back to localStorage
+   // localStorage.setItem('appointments', JSON.stringify(existingAppointments));
+     axios.post("https://crudcrud.com/api/ce9a290d0b1a478cac3a4b9341e05ae7/appointmentData",appointment)
+     .then((response)=>{
+        displayAppointments(response.data)
+     })
+     .catch((err)=>{
+        console.log(err)
+     })
+    // Optional: Display a success message or update the UI
+    alert('Appointment booked successfully!');
+    //displayAppointments()
+});
+
+function displayAppointments() {
+    const appointmentsList = document.getElementById('appointmentsList');
+    appointmentsList.innerHTML = ''; // Clear the existing content
+
+    const appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+
+    appointments.forEach((appointment, index) => {
+        const appointmentDiv = document.createElement('div');
+        appointmentDiv.classList.add('appointment-item');
+
+        // Display appointment details
+        appointmentDiv.innerHTML = `
+            <p><strong>Name:</strong> ${appointment.name}</p>
+            <p><strong>Email ID:</strong> ${appointment.email}</p>
+            <p><strong>Phone:</strong> ${appointment.phone}</p>
+            
+        `;
+
+        appointmentsList.appendChild(appointmentDiv);
+    });
+}
