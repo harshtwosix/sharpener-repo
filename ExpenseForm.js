0e506258b57153b9f91c1eeea39a15fbfd2b48e6
Harshit <harshraj02062000@gmail.com>
2024-02-27 12:45:14 +0530

    added user input console log functionality using single useState implementation

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,13 +1,63 @@
 import './ExpenseForm.css'
-import React  from 'react';
+import React,{useState}  from 'react';
 
 
 function ExpenseForm() {
+    /*
+    const [enteredTitle,setEnteredTitle]=useState('')
+    const [enteredAmount,setEnteredAmount]=useState('')
+    const [enteredDate,setEnteredDate]=useState('')
     const titleChangeHandler=(event)=>{
-        console.log(event.target.value)
+        setEnteredTitle(event.target.value)
     }
+    const amountChangeHandler=(event)=>{
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler=(event)=>{
+        setEnteredDate(event.target.value)
+    }
+    */
+     //OR WE CAN USE useState ONE TIME IN FOLLOWING SYNTAX
+   const [userInput,setUserInput]=useState({
+       enteredTitle:'',
+       enteredAmount:'',
+       enteredDate:''
+   })
+    const titleChangeHandler=(event)=>{
+        setUserInput({
+            ...userInput,
+            enteredTitle:event.target.value,
+        })
+    }
+    const amountChangeHandler=(event)=>{
+        setUserInput({
+            ...userInput,
+            enteredAmount:event.target.value,
+        })
+    }
+    const dateChangeHandler=(event)=>{
+        setUserInput({
+            ...userInput,
+            enteredDate:event.target.value,
+        })
+    }
+
+    const submiteventhandler=(event)=>{
+       event.preventDefault()
+
+       const expenseData={
+        title:userInput.enteredTitle,
+        amount:userInput.enteredAmount,
+        date:new Date(userInput.enteredDate)
+       }
+        console.log('clicking')
+       console.log(expenseData);
+    }
+    
+    
+  
     return (<div>
-        <form>
+        <form onSubmit={submiteventhandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__controls'>
                     <label>Title</label>
@@ -15,17 +65,18 @@ function ExpenseForm() {
                 </div>
                 <div className='new-expense__controls'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' onChange={amountChangeHandler} min='0.01' step='0.01' />
                 </div>
                 <div className='new-expense__controls'>
-                    <label>Date</label>
-                    <input type='date' min='2019-1-1' max='2022-12-31' />
+                    <label>Date </label>
+                    <input type='date' onChange={dateChangeHandler} min='2019-1-1' max='2022-12-31' />
                 </div>
             </div>
-        </form>
-        <div className='new-expense__actions'>
+            <div className='new-expense__actions'>
             <button type='submit'>Submit</button>
         </div>
+        </form>
+        
     </div>
     )
 }
